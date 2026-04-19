5091eb9daf53da3324669e6693ecfbd1b2396151
shaoyihao <shaoyihao0517@qq.com>
2026-04-19 07:47:18 +0000

    modify MInode structure, some code refactored

--- junction/fs/shaofs/inode.cc
+++ junction/fs/shaofs/inode.cc
@@ -1,20 +1,14 @@
 #include "fs.h"
+#include "inode.h"
 
 int alloc_inum()
 {
-	// for (int idx = 0; idx < INODENUM; idx++)
-	// 	if (!bitmap_atomic_test_and_set(imap, idx)) return idx;
-
-	static volatile int cursor = 0;
-  	int start = atomic_read(&cursor);
+	static volatile unsigned int cursor = 0;
+	unsigned int start = __atomic_fetch_add(&cursor, 1, __ATOMIC_RELAXED);
   	for (int i = 0; i < INODENUM; i++)
   	{
-    	int idx = (start + i) % INODENUM;
-    	if (!bitmap_atomic_test_and_set(imap, idx))
-    	{
-      		atomic_write(&cursor, (idx + 1) % INODENUM);
-      		return idx;
-    	}
+    	unsigned int idx = (start + i) % INODENUM;
+    	if (!bitmap_atomic_test_and_set(imap, idx)) return idx;
   	}
 	
 	log_info("[ERROR] alloc_inum(): No free inode!");
@@ -24,4 +18,14 @@ void free_inum(int inum)    // 释放 inode，好像很少有场景需要 free�
 {
     if (inum < 0 || inum >= INODENUM) return;
     bitmap_atomic_clear(imap, inum);
+}
+
+bool uses_indirect_block(const DInode* ino) 
+{
+    if (ino->direct_extents[DIRECT_EXTENT_NUM - 1].block_count == 0) return false;  // 如果第 6 个槽位都是空的，连直接 Extent 都没用完，绝对不可能用间接块
+    
+	const iExtent& last_direct = ino->direct_extents[DIRECT_EXTENT_NUM - 1];
+    uint64_t max_direct_lblk = last_direct.logical_start + last_direct.block_count;   // 获取第 6 个 Extent 的覆盖范围
+    uint64_t required_lblks = (ino->file_size + BLOCK_SIZE - 1) / BLOCK_SIZE;  // 计算文件真实需要的逻辑块数量 (file_size 是精确维护的，即使有文件空洞，file_size 也会撑大)
+    return required_lblks > max_direct_lblk;   // 如果文件需要的逻辑块 超出了 直接 Extent 所能覆盖的最大逻辑块，即说明数据必然溢出到了间接块中。
 }
--- junction/fs/shaofs/inode.h
+++ junction/fs/shaofs/inode.h
@@ -4,15 +4,33 @@
 struct MInode : public DInode
 {
     mutable rwmutex_t dir_mtx;     // 若这个 inode 是目录，读写锁保护目录的并发访问
-    mutable iExtent extent_hint;   // 上次 extent 查找的缓存，加速顺序访问 O(1) 命中
+    mutable iExtent extent_hint;   // 上次访问的 extent，加速顺序访问
+    spinlock_t hint_lock;          // 保护多线程对 extent_hint 的更新 （线程在持有 MInode 读锁的情况下，也可以更新 hint）
 
-    MInode() { rwmutex_init(&dir_mtx); memset(&extent_hint, 0, sizeof(extent_hint)); }
+    MInode() 
+    { 
+        rwmutex_init(&dir_mtx); 
+        memset(&extent_hint, 0, sizeof(extent_hint)); 
+        spin_lock_init(&hint_lock);
+    }
     MInode& operator=(const DInode& disk_inode)   // 自定义拷贝赋值运算符，只拷贝盘上数据即可
     {
         DInode::operator=(disk_inode);
+        memset(&extent_hint, 0, sizeof(extent_hint));
+        spin_lock_init(&hint_lock);
         return *this;
     }
 };
 
 int alloc_inum();
 void free_inum(int inum);
+bool uses_indirect_block(const DInode* ino);
+
+static inline void update_extent_hint(MInode* inode, const iExtent& ext) 
+{
+    if (spin_try_lock(&inode->hint_lock)) 
+    {
+        inode->extent_hint = ext;
+        spin_unlock(&inode->hint_lock);
+    }
+}
